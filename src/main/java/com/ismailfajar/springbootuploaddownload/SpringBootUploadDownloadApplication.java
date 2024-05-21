06212aeefca3698c8b799ff26b255912337dd009
ismailfajar81 <ismailfajar81@.com>
2024-05-21 18:38:21 +0700

    Initial Commit

---
+++ src/main/java/com/ismailfajar/springbootuploaddownload/SpringBootUploadDownloadApplication.java
@@ -0,0 +1,13 @@
+package com.ismailfajar.springbootuploaddownload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUploadDownloadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootUploadDownloadApplication.class, args);
+    }
+
+}
